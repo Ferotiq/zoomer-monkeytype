2c2c7744b75cae21538de279be58bd3da823f5ca
Ferotiq <evanrayweigel@outlook.com>
2022-08-19 16:24:04 -0500

    use default import

--- packages/client/src/index.tsx
+++ packages/client/src/index.tsx
@@ -5,7 +5,7 @@ import { render } from "solid-js/web";
 import "./styles/index.scss";
 
 // components
-import { App } from "./app";
+import App from "./app";
 
 if (window.location.hostname === "127.0.0.1") {
   window.location.replace("http://localhost:3000/");
