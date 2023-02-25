959e65e38439ad291599375a3f8927f99ab598c6
Evan <evanrayweigel@outlook.com>
2023-02-25 16:22:11 -0600

    use global prisma client session

--- packages/website/src/db/use-user.tsx
+++ packages/website/src/db/use-user.tsx
@@ -1,11 +1,11 @@
-import { PrismaClient, User } from "@prisma/client";
+import { User } from "@prisma/client";
 import { Resource } from "solid-js";
 import { createServerData$, redirect } from "solid-start/server";
+import { db } from ".";
 import { getUser } from "./session";
 
 export const useUser = (): Resource<User | undefined> =>
 	createServerData$(async (_, { request }) => {
-		const db = new PrismaClient();
 		const user = await getUser(db, request);
 
 		if (!user) {
