483d8cf3b6c1e67f50321c658fcadfbe3d1ef3a7
Evan <evanrayweigel@outlook.com>
2023-03-30 11:30:39 -0500

    add all props to button

--- packages/website/src/components/button/index.tsx
+++ packages/website/src/components/button/index.tsx
@@ -1,10 +1,14 @@
 import type { JSX } from "solid-js";
 import styles from "./button.module.scss";
 
-interface ButtonProps {
+interface ButtonProps extends JSX.HTMLAttributes<HTMLDivElement> {
 	children: JSX.Element;
 }
 
 export function Button(props: ButtonProps): JSX.Element {
-	return <div class={styles.button}>{props.children}</div>;
+	return (
+		<div {...props} class={styles.button}>
+			{props.children}
+		</div>
+	);
 }
