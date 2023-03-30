e69b25e47cb81cf9d8f6aa0296089524ec447d6f
Evan <evanrayweigel@outlook.com>
2023-03-30 11:42:19 -0500

    add active and disabled props

--- packages/website/src/components/button/index.tsx
+++ packages/website/src/components/button/index.tsx
@@ -1,11 +1,22 @@
 import type { JSX } from "solid-js";
 import styles from "./button.module.scss";
 
-type ButtonProps = JSX.HTMLAttributes<HTMLDivElement>;
+interface ButtonProps extends JSX.HTMLAttributes<HTMLDivElement> {
+	disabled?: boolean;
+	active?: boolean;
+}
 
 export function Button(props: ButtonProps): JSX.Element {
 	return (
-		<div {...props} classList={{ ...props.classList, [styles.button]: true }}>
+		<div
+			{...props}
+			classList={{
+				...props.classList,
+				[styles.disabled]: props.disabled ?? false,
+				[styles.active]: props.active ?? false,
+				[styles.button]: true,
+			}}
+		>
 			{props.children}
 		</div>
 	);
